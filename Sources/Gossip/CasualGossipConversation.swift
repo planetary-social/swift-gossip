998c9cda9be2b4287b07cf8eb46b9f3004520a8c
月光虹 <iris@random.town>
2020-08-21 17:12:18 +0200

    Implement Casual Gossip Conversation as a wrapper around the incoming and outgoing streams
    Deliberately kept abstract.

--- Sources/Gossip/CasualGossipConversation.swift
+++ Sources/Gossip/CasualGossipConversation.swift
@@ -1,12 +1,28 @@
+import Foundation
+import Combine
 
 ///
 
 public class CasualGossipConversation {
+    
+    ///
+    
+    internal let incoming: AnyPublisher<Data, Never>
 
     ///
     
-    public init() {
-        #warning("todo")
+    internal let outgoing = PassthroughSubject<Data, Never>()
+
+    ///
+    
+    internal let services: [Cancellable]
+
+    ///
+    
+    public init<Incoming>(from incoming: Incoming, sendingWith send: @escaping (Data) -> Void)
+    where Incoming: Publisher, Incoming.Output == Output, Incoming.Failure == Never {
+        self.incoming = incoming.eraseToAnyPublisher()
+        self.services = [outgoing.sink { packet in send(packet) }]
     }
     
 }
