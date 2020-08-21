136f514beb207483198cd04e6662e9abbd2cdab8
月光虹 <iris@random.town>
2020-08-21 16:03:29 +0200

    WIP: Temporarily remove NIO-specific init from Blabber

--- Sources/Gossip/Blabber.swift
+++ Sources/Gossip/Blabber.swift
@@ -1,24 +1,11 @@
-import NIO
-
 ///
 
 public class Blabber {
-
-    ///
-    
-    public let endpoint: SocketAddress
-
-    ///
-    
-    private let group: EventLoopGroup
     
     ///
     
-    public init(at endpoint: SocketAddress, within group: EventLoopGroup) {
-        self.endpoint = endpoint
-        self.group = group
-        
-        #warning("todo: setup")
+    public init() {
+        #warning("todo")
     }
 
 }
