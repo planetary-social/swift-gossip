ad81639b4b0e2c475cb832a8805ab73b0661fcfb
月光虹 <iris@random.town>
2020-08-21 13:19:23 +0200

    Specify Quidnunc to be a Connectable Publisher
    Following the trope of Quidnunc being a Conversation, here we state that it's one that can be previously establish by means of connection.

---
+++ Sources/Gossip/Quidnunc+ConnectablePublisher.swift
@@ -0,0 +1,15 @@
+import Foundation
+import Combine
+
+///
+
+extension Quidnunc: ConnectablePublisher {
+    
+    ///
+    
+    public func connect() -> Cancellable {
+        #warning("todo")
+        return self
+    }
+    
+}
