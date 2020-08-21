4d29e832a331a7490711447b2ee6eb6298abd6fa
月光虹 <iris@random.town>
2020-08-21 13:02:20 +0200

    Specify the Casual Gossip Conversation to be a Subject accepting Data messages

---
+++ Sources/Gossip/CasualGossipConversation+Subject.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Combine
+
+///
+
+extension CasualGossipConversation: Subject {
+
+    ///
+    
+    public func send(_ value: Data) {
+        #warning("todo")
+    }
+    
+    ///
+    
+    public func send(completion: Subscribers.Completion<Never>) {
+        #warning("todo")
+    }
+    
+    ///
+    
+    public func send(subscription: Subscription) {
+        #warning("todo")
+    }
+    
+}
